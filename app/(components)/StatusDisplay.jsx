66d75994c138b248f58a49415c9996fd236f4072
NotAScratch <121782740+NotAScratch@users.noreply.github.com>
2024-02-07 20:29:57 +0545

    minor changes

---
+++ app/(components)/StatusDisplay.jsx
@@ -0,0 +1,10 @@
+
+const StatusDisplay = () => {
+  return (
+        <span className="inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200"> 
+
+        </span>
+    )
+}
+
+export default StatusDisplay
--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -1,13 +1,26 @@
 import DeleteBlock from './DeleteBlock'
 import PriorityDisplay from './PriorityDisplay'
 import ProgressDisplay from './ProgressDisplay';
+import StatusDisplay from './StatusDisplay';
 
 const TicketCard = () => {
   return (
-    <div>
-        <DeleteBlock />
+    <div className='flex flex-col bg-card hoverbg-card-hover rounded-md shadow-lg p-3 m-2'>
+        <div className='flex mb-3'>
         <PriorityDisplay/>
+        <div className='ml-auto'>
+        <DeleteBlock />
+        </div>
+        </div>
+        <h4>Ticket Title</h4>
+        <hr className='h-px' border-0 bg-page mb-2/>
+        <p className='whitespace-pre-wrap'>This is the ticket description</p>
+        <div className='flex-grow'></div>
+        <div className='flex mt-2'>
+          <div className='flex flex-col'
         <ProgressDisplay />
+        <StatusDisplay />
+        </div>
     </div>
   )
 }
