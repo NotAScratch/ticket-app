125ef35acce202a7e8cac54253563c24cf25bb4a
NotAScratch <121782740+NotAScratch@users.noreply.github.com>
2024-02-08 12:11:01 +0545

    MInor improvment in hover

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -5,21 +5,25 @@ import StatusDisplay from './StatusDisplay';
 
 const TicketCard = () => {
   return (
-    <div className='flex flex-col bg-card hoverbg-card-hover rounded-md shadow-lg p-3 m-2'>
-        <div className='flex mb-3'>
-        <PriorityDisplay/>
-        <div className='ml-auto'>
-        <DeleteBlock />
+    <div className="flex flex-col hover:bg-card-hover bg-card rounded-md shadow-lg p-3 m-2">
+      <div className="flex mb-3">
+        <PriorityDisplay />
+        <div className="ml-auto">
         </div>
-        </div>
-        <h4>Ticket Title</h4>
-        <hr className='h-px' border-0 bg-page mb-2/>
-        <p className='whitespace-pre-wrap'>This is the ticket description</p>
-        <div className='flex-grow'></div>
-        <div className='flex mt-2'>
-          <div className='flex flex-col'
-        <ProgressDisplay />
-        <StatusDisplay />
+      </div>
+        <h4 className="mb-1">Ticket Title</h4>
+        <hr className="h-px  border-0 bg-page mb-2 "></hr>
+        <p className="whitespace-pre-wrap"> This is the Ticket description</p>
+
+        <div className="flex-grow"></div>
+        <div className="flex mt-2">
+          <div className="flex flex-col">
+            <p className="text-xs  my-1">02/08/2024</p>
+            <ProgressDisplay />
+          </div>
+          <div className="ml-auto  flex items-end">
+            <StatusDisplay/>
+          </div>
         </div>
     </div>
   )
--- app/page.jsx
+++ app/page.jsx
@@ -2,10 +2,12 @@ import TicketCard from "./(components)/TicketCard"
 
 const Dashboard = () => {
   return (
-    <div className="flex-initial">
+    <div className="p-5" >
+      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
       <TicketCard/>
       <TicketCard/>
       <TicketCard/>
+      </div>
     </div>
   )
 }
